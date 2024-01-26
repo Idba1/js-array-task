06d4b0a6d6d3cf8b92cf2e4cebdaac1cdfcc8e70
Idba1 <moniraislamself@gmail.com>
2024-01-26 19:51:20 +0600

    practice task - 1

---
+++ practice-1.js
@@ -0,0 +1,17 @@
+/**
+ * 1. Declare an array
+ *Declare an array with 5 elements containing fruits
+ *console log the 3rd index element
+ *change the value of the 2nd index element to jambura
+ *console log the final array
+**/
+
+
+
+var fruits = ['mango', 'banana', 'apple', 'orange', 'graph'];
+console.log(fruits[3]);
+// newFruits = fruits[4];
+// console.log(newFruits);
+ fruits[2] = 'jambura';
+ console.log(fruits[2]);
+console.log(fruits);
