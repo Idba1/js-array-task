34e9fc1d287a23b6b95adc12c5e32cc25bb38141
Idba1 <moniraislamself@gmail.com>
2024-01-27 06:52:48 +0600

    update file name and solve provlem 4

---
+++ is-array.js
@@ -0,0 +1,22 @@
+/**
+ * 4. Checking if it's an Array
+Instructions:
+
+*Create different variables, each containing either an array or a non-array value.
+*Now use isArray to check if each variable is an array.
+(Print a message to the console indicating whether each variable is an array or not.
+ **/
+
+
+    let fruits = ['mango', 'banana'];
+    console.log(Array.isArray(fruits));
+
+    let touristDestinations = ['Nepal'];
+    console.log(Array.isArray(touristDestinations));
+
+    let bookList = ['Opekkha'];
+    console.log(Array.isArray(bookList));
+
+    let name = 'Kona';
+    console.log(Array.isArray(name));
+
