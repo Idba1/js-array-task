af216448cc917f5e1c3be23b2ea8d0f144a56f7b
Idba1 <moniraislamself@gmail.com>
2024-01-27 06:57:46 +0600

    update file name and solve problem 5

---
+++ concat.js
@@ -0,0 +1,15 @@
+/**
+ * 5. Combining Arrays
+Instructions:
+*Create two arrays of your choice.
+*Use the concat method to combine the two arrays into a new array.
+*Print both the original arrays and the combined array using console.log().
+ **/
+
+
+let bookList = ['Opekkha', 'Vallage Na', 'Javascript', 'Habluder Jonno Programming'];
+console.log(bookList);
+let fruits = ['mango', 'banana', 'apple', 'orange', 'graph'];
+console.log(fruits);
+let newArray = bookList.concat(fruits);
+console.log(newArray);
